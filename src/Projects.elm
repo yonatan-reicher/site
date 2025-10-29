e382c5b4855c5f2a73f1b5b08f1f3042f2d9148f
Jonathan Reicher <yony252525@gmail.com>
2025-10-29 20:22:30 +0200

    Redo projects

--- src/Projects.elm
+++ src/Projects.elm
@@ -4,16 +4,7 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Browser exposing (Document)
-import Links exposing (myGithub)
 import Common exposing (myStylesheet)
-
-
-main = Browser.document
-    { init = init
-    , update = update
-    , view = view
-    , subscriptions = subscriptions
-    }
 
 
 type alias Flags = ()
@@ -25,18 +16,207 @@ type alias Model = ()
 type alias Msg = ()
 
 
-init : Flags -> (Model, Cmd Msg)
+init : Flags -> ( Model, Cmd Msg )
 init () =
     ( ()
     , Cmd.none
     )
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
-update () () =
-    ( ()
-    , Cmd.none
-    )
+update msg model = ( model, Cmd.none )
+
+
+type alias Project =
+    { title: String
+    , description: List (Html Msg)
+    , date: String
+    , githubLink: Maybe String
+    , picturePath: Maybe String
+    }
+
+
+viewProjectLink : Project -> Html Msg
+viewProjectLink p =
+    case p.githubLink of
+        Nothing -> text ""
+        Just l -> a [ href l ] [ text "GitHub" ]
+
+
+viewProjectPicture : Project -> Html Msg
+viewProjectPicture p =
+    case p.picturePath of
+        Nothing -> text ""
+        Just path ->
+            a
+                [ href path ]
+                [ img
+                    [ src path
+                    , style "width" "100%"
+                    ]
+                    []
+                ]
+
+
+viewProject : Project -> Html Msg
+viewProject proj =
+    div 
+        [ class "project"
+        ]
+        [ div
+            [ style "display" "inline-block"
+            , style "width" "70%"
+            , style "vertical-align" "top"
+            ]
+            [ h3 [ style "margin-top" "0" ] [ text proj.title ]
+            , span
+                [ style "display" "flex"
+                , style "flex-direction" "row"
+                , style "gap" "10px"
+                ]
+                [ span [] [ text proj.date ]
+                , viewProjectLink proj
+                ]
+            , p [] proj.description
+            ]
+        , div
+            [ style "display" "inline-block"
+            , style "width" "30%"
+            ]
+            [ viewProjectPicture proj
+            ]
+        ]
+
+
+satellai : Project
+satellai =
+    { title = "SatellAI Project"
+    , description = [ text """
+        SatellAI is the name of a team I was briefly appended to for a summer
+        project. The project was about using classical machine learning
+        techniques on time-series data. Unfortunately I cannot say more.
+    """ ]
+    , date = "2024"
+    , githubLink = Nothing
+    , picturePath = Nothing
+    }
+
+
+tekclinic : Project
+tekclinic =
+    { title = "TekClinic"
+    , description = [ text """
+        A final-year group project full stack application.
+    """ ]
+    , date = "2024-2025"
+    , githubLink = Just "https://www.github.com/tekclinic"
+    , picturePath = Nothing
+    }
+
+
+affogato : Project
+affogato =
+    { title = "Affogato"
+    , description = [ text """
+        A machine learning library for python, made with my friend """, a [ href
+        "https://github.com/galord123" ] [ text "Gal" ], text """.
+        It implements FC layers, Convolution layers, various activation
+        functions, SGD, Adam, Softmax, custom training loops and more.
+        Everything is modular
+    """ ]
+    , date = "2021-2022"
+    , githubLink = Just "https://gitlab.com/affogato/affogato"
+    , picturePath = Just "images/affogato.png"
+    }
+
+
+waveFunctionCollapse : Project
+waveFunctionCollapse =
+    { title = "Wave Function Collapse"
+    , description = [ text """
+        Demo implementation of the Wave Function Collapse algorithm to generate
+        interesting looking procedural environments. The demo shows off a cute
+        townscape.
+        Made in C# with MonoGame.
+    """ ]
+    , date = "2023"
+    , githubLink = Just "https://github.com/yonatan-reicher/wave-function-collapse/"
+    , picturePath = Just "images/wave-function-collapse.png"
+    }
+
+
+grRedesign : Project
+grRedesign =
+    { title = "GR++ Redesign"
+    , description =
+        [ text """
+            A redesign of the website of the Technion's Computer Science
+            faculty. Made in about a weekend, but got improved over time.
+            (Works on both
+        """
+        , a 
+            [ href "https://chrome.google.com/webstore/detail/gr%20%20-redesign/iaagdkjkkpiiollookginpcjapbhjfli" ]
+            [ text "Chrome" ]
+        , text " and "
+        , a 
+            [ href "https://addons.mozilla.org/en-US/firefox/addon/gr-redesign/" ]
+            [ text "Firefox" ]
+        , text ")."
+        ]
+    , date = "2023"
+    , githubLink = Just "https://github.com/yonatan-reicher/gr-redesign"
+    , picturePath = Just "images/grpp.jpg"
+    }
+
+
+ab : Project
+ab =
+    { title = "Compiler"
+    , description = [ text """
+        A compiler for a C-style language. Makes runnable x86 assembly source
+        code to be used with TASM.
+    """ ]
+    , githubLink = Just "https://github.com/yonatan-reicher/AB"
+    , picturePath = Just "images/ab.png"
+    , date = "2019"
+    }
+
+
+blobs : Project
+blobs =
+    { title = "Blobs"
+    , description =
+        [ text """
+            A 2D simulation of blob creatures. They move, they fight and eat
+            each other, and they have color-based sight.
+        """
+        , ul []
+            [ li [] [ text "Implemented in Rust using RayLib" ]
+            , li [] [ text """
+                Collision detection implemented by me using sweep and prune.
+            """ ]
+            ]
+        ]
+    , date = "2021"
+    , githubLink = Just "https://github.com/yonatan-reicher/blobs"
+    , picturePath = Just "images/blobs.jpg"
+    }
+
+
+viewContent : Html Msg
+viewContent =
+    div
+        [ style "max-width" "1000px"
+        , style "margin" "0 auto"
+        ]
+        [ h1 [] [ text "Projects" ]
+        , viewProject satellai
+        , viewProject tekclinic
+        , viewProject affogato
+        , viewProject waveFunctionCollapse
+        , viewProject grRedesign
+        , viewProject ab
+        , viewProject blobs
+        ]
 
 
 view : Model -> Document Msg
@@ -44,262 +224,18 @@ view () =
     { title = "Jonathan Reicher | Projects"
     , body =
         [ myStylesheet
-        , viewProjects
+        , viewContent
         ]
     }
 
 
-viewProjects : Html Msg
-viewProjects =
-    div
-        [ class "section"
-        , class "projects"
-        ]
-        [ h2
-            [ class "section-title"
-            ]
-            [ text "Projects"
-            ]
-        , ul
-            [ class "projects-list"
-            ]
-            [ viewProject
-                { name = "Affogato"
-                , description =
-                    [ text
-                        """
-                        A simple, yet powerful, machine learning library for
-                        python. Made with love, with my friend, 
-                        """
-                    , a 
-                        [ href "https://github.com/galord123" ]
-                        [ text "Gal"
-                        ]
-                    , text
-                        """
-                        , For the Magshimim project.
-                        """
-                    ]
-                , link = "https://gitlab.com/affogato/affogato"
-                , picturePath = Just "images/affogato.png"
-                , date = "2022"
-                }
-            , viewProject
-                { name = "Wave Function Collapse"
-                , description =
-                    [ text
-                        """
-                        A simple demo implementation of the Wave Function
-                        Collapse algorithm to generate an interesting looking
-                        town!
-                        Made in C# with MonoGame.
-                        """
-                    ]
-                , link = "https://github.com/yonatan-reicher/wave-function-collapse/"
-                , picturePath = Just "images/wave-function-collapse.png"
-                , date = "2023-12-18"
-                }
-            , viewProject
-                { name = "Character Controller Test"
-                , description =
-                    [ text "A simple character controller to test out phaser.js"
-                    ]
-                , link = "https://yonatan-reicher.github.io/phaser-character-test/"
-                , picturePath = Just "images/phaser-character-test.png"
-                , date = "2023-10-27"
-                }
-            , viewProject
-                { name = "GR++ Redesign"
-                , description =
-                    [ text
-                        """
-                        Major redesign for the website of the fauclty of
-                        computer science in the Technion. Made with love, in
-                        about a weekend. A must for every student in the
-                        faculty! (Works on both
-                        """
-                    , a 
-                        [ href "https://chrome.google.com/webstore/detail/gr%20%20-redesign/iaagdkjkkpiiollookginpcjapbhjfli" ]
-                        [ text "Chrome"
-                        ]
-                    , text " and "
-                    , a 
-                        [ href "https://addons.mozilla.org/en-US/firefox/addon/gr-redesign/"
-                        ]
-                        [ text "Firefox"
-                        ]
-                    , text ")."
-                    ]
-                , link = "https://addons.mozilla.org/en-US/firefox/addon/gr-redesign/"
-                , picturePath = Just "images/grpp.jpg"
-                , date = "2023-02-24"
-                }
-            , viewProject
-                { name = "AB"
-                , description =
-                    [ text
-                        """
-                        A C-Style language that complies to human-readable
-                        X86 assembly for TASM
-                        """
-                    ]
-                , link = "https://github.com/yonatan-reicher/AB"
-                , picturePath = Just "images/ab.png"
-                , date = "2019"
-                }
-            , viewProject
-                { name = "Blobs"
-                , description =
-                    [ p []
-                        [ text
-                            """
-                            2d simulation of blob creatures. They have
-                            color-based sight, and can move, eat, and fight.
-                            """
-                        ]
-                    , p []
-                        [ ul []
-                            [ li [] [ text
-                                """
-                                Implemented using Rust and Raylib.
-                                """ ]
-                            , li [] [ text 
-                                """
-                                Collision detection implemented by me with the
-                                sweep and prune algorithm.
-                                """ ]
-                            ]
-                        ]
-                    ]
-                , link = "https://github.com/yonatan-reicher/blobs"
-                , picturePath = Just "images/blobs.jpg"
-                , date = "2021-01-18"
-                }
-            , viewProject
-                { name = "This Site"
-                , description =
-                    [ text
-                        """
-                        Putting stuff on the internet is fun, and also - really
-                        embarrassing.
-                        """
-                    , br [] []
-                    , br [] []
-                    , text
-                        """
-                        I made this site to put my projects on display, to
-                        practice some code, and to get over a fear of putting
-                        my work out there.
-                        """
-                    ]
-                , link = "#"
-                , picturePath = Nothing
-                , date = "2022-01-28"
-                }
-            ]
-        , p [ style "text-align" "center" ]
-            [ text
-                """
-                As you might have noticed, all of these are old. One day I'll update this site.
-                The rest of my projects are on
-                """
-            , a [ href myGithub ] [ text "github" ]
-            , text "."
-            ]
-        ]
+subscriptions model = Sub.none
 
 
-viewProject :
-    { name: String, description: List (Html Msg)
-    , link: String, picturePath: Maybe String
-    , date: String
-    } -> Html Msg
-viewProject { name, description, link, picturePath, date } =
-    li
-        [ class "project"
-        ]
-        [ a [ href link ]
-            [ h3 [ class "project-title" ] [ text name ]
-            ]
-        , case picturePath of
-            Just path ->
-                a
-                    [ href link
-                    , target "blank"
-                    ]
-                    [ img
-                        [ class "project-image"
-                        , src path
-                        ]
-                        []
-                    ]
-            Nothing -> text ""
-        , p
-            [ class "project-description"
-            ]
-            description
-        , p
-            [ class "project-date"
-            ]
-            [ text date
-            ]
-        ]
-
-
-viewAbout : Html Msg
-viewAbout =
-    div
-        [ class "section"
-        , class "about"
-        ]
-        [ h2
-            [ class "section-title"
-            ]
-            [ text "About"
-            ]
-        , p
-            [ class "about-text"
-            ]
-            [ text
-                """
-                Hi, I'm Jonathan Reicher.
-                I'm studying to become a software engineer.
-                Currently, I am earning credit for a degree in Computer Scienece,
-                and in my last year of Magshimim.
-                I'm also 17 years old.
-                Right now I am working on a project called affogato
-                with my friend Gal.
-                I am currently in an application process for the IDF.
-                """
-            ]
-        ]
-
-
-viewContact : Html Msg
-viewContact =
-    div
-        [ class "section"
-        , class "contact"
-        ]
-        [ h2
-            [ class "section-title"
-            ]
-            [ text "Contact"
-            ]
-        , p
-            [ class "contact-text"
-            ]
-            [ text
-                """
-                You can contact me by email at yony252525@gmail.com or by phone
-                at 972-58-425-6935.
-                """
-            ]
-        ]
-
-
-subscriptions : Model -> Sub Msg
-subscriptions () =
-    Sub.none
-
-
+main =
+    Browser.document
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
